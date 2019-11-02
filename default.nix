55f6863fc54e7a793569cdae406bb1477bb2a136
Amy <amy@brickx.com>
2019-11-03 03:04:10 +1100

    Fix `amazonka` compilation error

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, amazonka, amazonka-core, amazonka-ec2
+, amazonka-opsworks, base, stdenv
+}:
 mkDerivation {
   pname = "cloud";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [
+    amazonka amazonka-core amazonka-ec2 amazonka-opsworks base
+  ];
   executableHaskellDepends = [ base ];
   doHaddock = false;
   license = "unknown";
--- shell.nix
+++ shell.nix
@@ -11,6 +11,17 @@ let
   pkgs = import src {};
   f = import ./default.nix;
   packageSet = pkgs.haskell.packages.${compiler};
-  drv = packageSet.callPackage f {};
+  hspkgs = 
+      packageSet.override {
+        overrides = (self: super: {
+          amazonka = pkgs.haskell.lib.doJailbreak super.amazonka;
+          amazonka-core = pkgs.haskell.lib.doJailbreak super.amazonka-core;
+          bloodhound = pkgs.haskell.lib.doJailbreak super.bloodhound;
+          katip-elasticsearch = pkgs.haskell.lib.dontCheck super.katip-elasticsearch;
+          ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+          ghcWithPackages = self.ghc.withPackages;
+        });
+      };
+  drv = hspkgs.callPackage f {};
 in
   if pkgs.lib.inNixShell then drv.env else drv
